8abfd676d6f4c0cb4edec53068e293235867baee
Alex Pogue <alex@alex-virtual-machine.(none)>
2013-03-12 22:45:27 -0500

    Add test prints and some small fixes

--- sndcat.c
+++ sndcat.c
@@ -3,6 +3,7 @@
 #include "fileUtils.h"
 
 void concatenateSounds(sound_t* s1, sound_t* s2, sound_t* dest, fileType_t resultType);
+void concatenateData(sound_t* s1, sound_t* s2, sound_t* dest);
 
 int main(int argc, char** argv) {
   if(argc < 2) {
@@ -56,9 +57,21 @@ int main(int argc, char** argv) {
       sounds[i] = loadSound(fp, fileNames[i]);
       fclose(fp);
       if(i >= 1) {
-        /*concatenateSounds(sounds[i-1], sounds[i]);*/
+        /*concatenateSounds(sounds[i-1], sounds[i], dest, CS229);*/
       }
     }
+    sound_t* dest = loadEmptySound();
+    concatenateSounds(sounds[0], sounds[1], dest, CS229);
+    char* data = (char*)dest->rawData;
+    printf("s1 dataSize = %d\n", sounds[0]->dataSize);
+    printf("s2 dataSize = %d\n", sounds[1]->dataSize);
+    printf("dest dataSize = %d\n", dest->dataSize);
+    /*
+    for(i = 0; i < calculateNumSamples(dest); i++) {
+      printf("%d\n", data[i]);
+    }
+    */
+
     for(i = 0; i < numFiles; i++) {
       unloadSound(sounds[i]);
     }
@@ -69,7 +82,6 @@ int main(int argc, char** argv) {
 /* concatenate sounds and store the concatenated sound into dest. Use the
   format specified by resultType */
 void concatenateSounds(sound_t* s1, sound_t* s2, sound_t* dest, fileType_t resultType) {
-  /* convert both to resultType */
   if(resultType == CS229) {
     waveToCs229(s1);
     waveToCs229(s2);
@@ -83,22 +95,34 @@ void concatenateSounds(sound_t* s1, sound_t* s2, sound_t* dest, fileType_t resul
     return;
   }
   if(s1->bitDepth > s2->bitDepth) {    
+    dest->bitDepth = s1->bitDepth;
     scaleBitDepth(s1->bitDepth, s2);
   }
   else if(s1->bitDepth < s2->bitDepth) {
+     printf("s1 bitdepth = %d\n", s1->bitDepth);
+    printf("s2 bitdepth = %d\n", s2->bitDepth);
+   dest->bitDepth = s2->bitDepth;
     scaleBitDepth(s2->bitDepth, s1);
+    printf("s1 bitdepth = %d\n", s1->bitDepth);
+  }
+  else {
+    dest->bitDepth = s1->bitDepth;
   }
   if(s1->numChannels > s2->numChannels) {
+    dest->numChannels = s1->numChannels;
     addZeroedChannels(s1->numChannels - s2->numChannels, s2);
+    printf("numChannels = %d\n", s2->numChannels);
   }
   else if(s1->numChannels < s2->numChannels) {
+    dest->numChannels = s2->numChannels;
     addZeroedChannels(s2->numChannels - s1->numChannels, s1);
   }
-  /* concatenate rawData */
-  dest->bitDepth = s1->bitDepth;
+  else {
+    dest->numChannels = s1->numChannels;
+  }
+  concatenateData(s1, s2, dest);
   dest->sampleRate = s1->sampleRate;
   dest->fileType = resultType;
-  dest->numChannels = s1->numChannels;
 }
 
 void concatenateData(sound_t* s1, sound_t* s2, sound_t* dest) {
@@ -118,6 +142,7 @@ void concatenateData(sound_t* s1, sound_t* s2, sound_t* dest) {
     /* TODO: REMOVE THIS TEST PRINT */
     printf("We cannot cat these two sounds!\n");
   }
+
   s1CharData = (char*)s1->rawData;
   s2CharData = (char*)s2->rawData;
   destCharData = (char*)dest->rawData;
@@ -127,4 +152,5 @@ void concatenateData(sound_t* s1, sound_t* s2, sound_t* dest) {
   for(i = 0; i < s2->dataSize; i++) {
     destCharData[i + s1->dataSize] = s2CharData[i];
   }
+  dest->dataSize = newDataSize;
 }
